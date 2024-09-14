f66fb7faf2a0865b395ea8327289c7f530234bde
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-09-14 13:41:02 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <div>
       <h1>Shopping cart</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {getAllProducts &&
         getAllProducts.data &&
         getAllProducts.data.length > 0 ? (
--- src/components/product-card/index.js
+++ src/components/product-card/index.js
@@ -7,21 +7,32 @@ import {
   CardFooter,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "../ui/button";
 
 function ProductCard({ item }) {
   return (
     <Card>
       <CardContent>
-        <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
+        <div className="w-full aspect-w-8 aspect-h-8 lg:h-80">
           <img
             src={item?.thumbnail}
             alt={item?.title}
             className="h-full w-full object-cover object-top"
           />
         </div>
-        <CardTitle>{item?.title}</CardTitle>
-        <CardDescription>{item?.brand}</CardDescription>
-        <CardFooter>{`Price: $${item?.price}`}</CardFooter>
+        <div className="p-6">
+          <CardTitle className="text-lg font-bold text-gray-900">
+            {item?.title}
+          </CardTitle>
+          <div className="mt-4 justify-between flex items-centerflex-wrap gap-2">
+            <p className="text-lg font-extrabold text-gray-800">
+              {item?.price}
+            </p>
+            <div>
+              <Button>Details</Button>
+            </div>
+          </div>
+        </div>
       </CardContent>
     </Card>
   );
