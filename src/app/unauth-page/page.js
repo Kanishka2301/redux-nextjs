464f38796d1ddbb857d19a7c69affb1c436f30c9
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-09-15 23:01:34 +0530

    initial commit

--- src/app/unauth-page/page.js
+++ src/app/unauth-page/page.js
@@ -1,4 +1,10 @@
-function UnauthPage() {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+async function UnauthPage() {
+  const getSession = await auth();
+
+  if (getSession?.user) redirect("/");
   return (
     <div className="p-20">
       <h2 className="text-6xl font-extrabold">
